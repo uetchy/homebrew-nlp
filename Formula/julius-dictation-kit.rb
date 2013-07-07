09bfb7eebcb3c89a7554dbc6187cecfc5baab9ab
Yasuaki Uechi <uetchy@randompaper.co>
2013-07-07 17:46:16 +0900

    typo fixed

--- Formula/julius-dictation-kit.rb
+++ Formula/julius-dictation-kit.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class JuliusDictationKit < Formula
   homepage 'http://julius.sourceforge.jp/'
-  url 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fjulius%2F59050%2Fdictation-kit-v4.2.3.tar.gz'
+  url 'http://sourceforge.jp/frs/redir.php?f=/julius/59050/dictation-kit-v4.2.3.tar.gz'
   md5 '0de511ff01a2b45d6998b7b7f1d78db0'
 
   def install

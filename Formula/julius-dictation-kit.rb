b702e30c69042419566a65cd55470e008a1f3e3d
oame <oame@oameya.com>
2014-04-04 16:21:27 +0900

    Update version: 4.3.1

--- Formula/julius-dictation-kit.rb
+++ Formula/julius-dictation-kit.rb
@@ -2,20 +2,21 @@ require 'formula'
 
 class JuliusDictationKit < Formula
   homepage 'http://julius.sourceforge.jp/'
-  url 'http://sourceforge.jp/frs/redir.php?f=/julius/59050/dictation-kit-v4.2.3.tar.gz'
-  sha1 '39c66aa7d9ccf348467d38e0e5ebb3d946b577ff'
+  url 'http://sourceforge.jp/frs/redir.php?f=/julius/60416/dictation-kit-v4.3.1-osx.tgz'
+  sha1 '8b94b3dabf1ed27f94864e72767abb2fc274c5db'
 
   def install
-    prefix.install 'INSTALL.txt'
     prefix.install 'HOWTO.txt'
+    prefix.install 'HOWTO-dnn.txt'
     prefix.install 'TROUBLE.txt'
     prefix.install 'doc/LICENSE.txt'
     prefix.install 'doc/Release.txt'
     prefix.install 'doc/Release-ja.txt'
     prefix.install 'doc/Sample.jconf'
-    man.install Dir['doc/man/*']
-    share.install 'fast.jconf'
-    share.install 'light.jconf'
+    man.install Dir['doc/manuals/*']
+    share.install 'main.jconf'
+    share.install 'am-dnn.jconf'
+    share.install 'am-gmm.jconf'
     share.install 'model'
   end
 end

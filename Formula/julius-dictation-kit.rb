5dcc2a48d0955519f987e936be81c26cac8bc4c4
Yasuaki Uechi <uetchy@randompaper.co>
2014-08-03 19:10:59 +0900

    Install all of contents into share dir

--- Formula/julius-dictation-kit.rb
+++ Formula/julius-dictation-kit.rb
@@ -4,19 +4,14 @@ class JuliusDictationKit < Formula
   homepage 'http://julius.sourceforge.jp/'
   url 'http://sourceforge.jp/frs/redir.php?f=/julius/60416/dictation-kit-v4.3.1-osx.tgz'
   sha1 '3e9fe6edcc3647192369cc08ac1725618eeb1551'
+  version '4.3.1'
 
   def install
-    prefix.install 'HOWTO.txt'
-    prefix.install 'HOWTO-dnn.txt'
-    prefix.install 'TROUBLE.txt'
-    prefix.install 'doc/LICENSE.txt'
-    prefix.install 'doc/Release.txt'
-    prefix.install 'doc/Release-ja.txt'
-    prefix.install 'doc/Sample.jconf'
-    man.install Dir['doc/manuals/*']
-    share.install 'main.jconf'
-    share.install 'am-dnn.jconf'
-    share.install 'am-gmm.jconf'
-    share.install 'model'
+    share.install Dir['*'].reject{|f| f['src'] || f['bin']}
+    bin.install(
+      'bin/dnnclient-gpu.py',
+      'bin/dnnclient.py'
+    )
+    bin.install Dir['bin/scoring/*.pl']
   end
 end

46011b59b7e9001961f636fe401d56ac959033a0
Yasuaki Uechi <uetchy@randompaper.co>
2014-10-27 10:40:54 +0900

    Add homepage, create 'etc' dir

--- Formula/chasen.rb
+++ Formula/chasen.rb
@@ -1,17 +1,20 @@
 require "formula"
 
 class Chasen < Formula
-  homepage ""
-  url "http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fchasen-legacy%2F56305%2Fchasen-2.4.5.tar.gz"
+  homepage "http://chasen-legacy.sourceforge.jp/"
+  url "http://sourceforge.jp/frs/redir.php?f=/chasen-legacy/56305/chasen-2.4.5.tar.gz"
   sha1 "a07d7f423531d2c155384bc6de64d515c5f0ac1c"
 
-  depends_on 'darts'
+  depends_on "darts"
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
+
+    (prefix + "etc").mkdir
+    (prefix + "etc/README").write "Please remove after installation"
   end
 end

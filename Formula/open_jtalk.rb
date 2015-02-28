09edc3c8f37a58f63a17a0e350f7b626225500c1
Yasuaki Uechi <uetchy@randompaper.co>
2015-02-28 16:11:38 +0900

    Update version: 1.07

--- Formula/open_jtalk.rb
+++ Formula/open_jtalk.rb
@@ -2,20 +2,20 @@ require 'formula'
 
 class OpenJtalk < Formula
   homepage 'http://open-jtalk.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/open-jtalk/Open%20JTalk/open_jtalk-1.07/open_jtalk-1.07.tar.gz'
-  sha1 '792fe7ba9ce2189e67b2d6902147e560cb6d5446'
-  version '1.07'
+  url 'http://downloads.sourceforge.net/project/open-jtalk/Open%20JTalk/open_jtalk-1.08/open_jtalk-1.08.tar.gz'
+  sha1 '34749abce5f8263ebbe9843b92407f4f0a742c66'
+  version '1.08'
+
+  depends_on 'hts_engine_api'
 
   def install
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --with-charset=UTF-8
     ]
 
     system "./configure", *args
-
     system "make", "install"
   end
 end

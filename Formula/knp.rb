5c0d965be1740ef29c7d0a4daaea5a05f3ef3995
hi-konno <hi-konno@jp.toyota-itc.com>
2017-03-22 16:59:38 +0900

    Corresponds to knp 4.17 update.

--- Formula/knp.rb
+++ Formula/knp.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Knp < Formula
   homepage "http://nlp.ist.i.kyoto-u.ac.jp/index.php?KNP"
-  url "http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/knp/knp-4.16.tar.bz2&name=knp-4.16.tar.bz2"
-  sha256 "a500e04cbf581eda4c8d2be93d6eff9066069b6287a465b6c4050604bd5faeb5"
+  url "http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/knp/knp-4.17.tar.bz2&name=knp-4.17.tar.bz2"
+  sha256 "7f7c007b735b4ac2d93eb0958e8253d8d7b607ae93f2238747dbe8e4f15689ca"
 
   depends_on "juman"
   depends_on "tinycdb"

2f7f914bf403c355ce9a387d557948472c8bb7b3
shoma2da <marcos.shoichi.matsuda@gmail.com>
2017-10-06 15:46:22 +0900

    Corresponds to knp 4.18 update.

--- Formula/knp.rb
+++ Formula/knp.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Knp < Formula
   homepage "http://nlp.ist.i.kyoto-u.ac.jp/index.php?KNP"
-  url "http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/knp/knp-4.17.tar.bz2&name=knp-4.17.tar.bz2"
+  url "http://nlp.ist.i.kyoto-u.ac.jp/DLcounter/lime.cgi?down=http://nlp.ist.i.kyoto-u.ac.jp/nl-resource/knp/knp-4.18.tar.bz2&name=knp-4.18.tar.bz2"
   sha256 "7f7c007b735b4ac2d93eb0958e8253d8d7b607ae93f2238747dbe8e4f15689ca"
 
   depends_on "juman"
